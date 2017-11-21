7f489c807148122c66ae83640dbdd364a53a79d7
aparnawso2 <aparna@wso2.com>
2017-11-21 16:29:03 +0530

    added microservice sample

---
+++ src/main/java/org/wso2/chaosmonkey/ChaosService.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.chaosmonkey;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+/**
+ * Created by aparna on 11/21/17.
+ */
+
+@Path("/chaos")
+public class ChaosService {
+
+    @POST
+    @Path("/terminate/{instance}")
+    public String terminateServer(@PathParam("instance") String instance) {
+        System.out.println("Terminating instance : " + instance);
+        return "Done!!!";
+    }
+
+    @POST
+    @Path("/revive/{instance}")
+    public String reviveServer(@PathParam("instance") String instance) {
+        System.out.println("Starting server : " + instance);
+        return "Done!!!";
+    }
+}
